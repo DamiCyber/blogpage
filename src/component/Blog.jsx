95dd01591d1666a47ce3e6d0516129a22c44d2db
Damig <ifatifod4@gmail.com>
2026-02-21 16:13:15 +0000

    update

--- src/component/Blog.jsx
+++ src/component/Blog.jsx
@@ -82,7 +82,7 @@ const Blog = () => {
             </div>
           </div>
         ) : (
-          <div className=" grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
             {posts.map((post) => (
               <article 
                 key={post._id} 
